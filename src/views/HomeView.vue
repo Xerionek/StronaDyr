d47553d8a3667602a063685fee9dae117f6af63d
misiuz <michal2006-02@wp.pl>
2024-05-29 16:05:01 +0200

    Film w tle, zmiany w stylach

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="bg">
-    <main id="MainHome">
-      <img src="@/assets/WelcomeTxt.webp" alt="Powitanie">
-      <div class="wrapper">
-        <!-- <router-link to="/droga-krzyzowa-srodowisk-tworczych" class="btnHomeHref">{{ firstBtnTxt }}</router-link>
-        <router-link to="/zaduszki-jazzowe-srodowisk-tworczych" class="btnHomeHref">{{ secondBtnTxt }}</router-link> -->
-        <router-link to="/warsztaty-muzyki-liturgicznej" class="btnHomeHref">{{ thirdBtnTxt }}</router-link>
-      </div>
-    </main>
-  </div>
+  <video class="bg" src="@/assets/background-movie.mp4" autoplay muted loop></video>    
+  <main id="MainHome">
+    <img src="@/assets/WelcomeTxt.webp" alt="Powitanie">
+    <div class="wrapper">
+      <!-- <router-link to="/droga-krzyzowa-srodowisk-tworczych" class="btnHomeHref">{{ firstBtnTxt }}</router-link> -->
+      <!-- <router-link to="/zaduszki-jazzowe-srodowisk-tworczych" class="btnHomeHref">{{ secondBtnTxt }}</router-link> -->
+      <router-link to="/warsztaty-muzyki-liturgicznej" class="btnHomeHref">{{ thirdBtnTxt }}</router-link>
+    </div>
+  </main>
 </template>
 
 <script>
