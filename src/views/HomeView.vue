a53f1cd52d8bb9ad2564a8136d6c9d8f9d656421
Krystian <krystianwasiewicz@gmail.com>
2024-09-22 19:13:01 +0200

    dfgdfg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,11 @@
   <main id="MainHome">
     <img src="Welcometxt.webp" alt="Powitanie">
     <div class="wrapper">
-      <!-- <router-link to="/droga-krzyzowa-srodowisk-tworczych" class="btnHomeHref">{{ firstBtnTxt }}</router-link> -->
-      <!-- <router-link to="/zaduszki-jazzowe-srodowisk-tworczych" class="btnHomeHref">{{ secondBtnTxt }}</router-link> -->
+      <router-link to="/droga-krzyzowa-srodowisk-tworczych" class="btnHomeHref">{{ firstBtnTxt }}</router-link>
+      <router-link to="/zaduszki-jazzowe-srodowisk-tworczych" class="btnHomeHref">{{ secondBtnTxt }}</router-link>
       <router-link to="/warsztaty-muzyki-liturgicznej" class="btnHomeHref">{{ thirdBtnTxt }}</router-link>
+      <router-link to="/zaduszki-jazzowe-srodowisk-tworczych" class="btnHomeHref">{{ fourthBtnTxt }}</router-link>
+      <div class="contanctInfoBtn">Kontakt</div>
     </div>
   </main>
 </template>
@@ -18,6 +20,7 @@ export default {
       firstBtnTxt: "",
       secondBtnTxt: "",
       thirdBtnTxt: "",
+      fourthBtnTxt: "",
     }
   },
   mounted() {
@@ -28,6 +31,7 @@ export default {
         const fTarget = " Droga Krzyżowa \n Środowisk Twórczych ";
         const sTarget = " Zaduszki jazzowe \n Środowisk Twórczych ";
         const tTarget = " Warsztaty \n Muzyki Liturgicznej ";
+        const foTarget = " Spotkania \n formacyjne ";
         let currChar = 0;
         let interval = setInterval(() => {
           currChar++;
@@ -41,6 +45,9 @@ export default {
           }
           if (currChar < tTarget.length) {
             this.thirdBtnTxt = tTarget.slice(0, currChar);
+          }
+          if (currChar < foTarget.length) {
+            this.fourthBtnTxt = foTarget.slice(0, currChar);
           }
         }, 20);
       }
